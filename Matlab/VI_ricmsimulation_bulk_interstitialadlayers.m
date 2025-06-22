5b28dd72fcc7f3395244427af92da435fe9d80d1
Vaibhav Srivatsan Vaiyakarnam <99827309+ZarseemDyartes@users.noreply.github.com>
2025-06-21 17:43:10 -0700

    Update VI_ricmsimulation_bulk_interstitialadlayers.m

--- Matlab/VI_ricmsimulation_bulk_interstitialadlayers.m
+++ Matlab/VI_ricmsimulation_bulk_interstitialadlayers.m
@@ -1,4 +1,4 @@
-%% V: Head-Tail Simulation, Interstitial Adlayers
+%% VI: Bulk Simulation, Interstitial Adlayers
 clear; clc;
 
 %IRM Light Wavelength
@@ -26,7 +26,6 @@ r12 = (n1-n2)/(n1+n2); %Base adlayer to bilayer
 r23 = (n2-n3)/(n2+n3); %Bilayer to interstitial adlayer
 r32 = -r23; %Interstitial adlayer to bilayer
 r24 = (n2-n4)/(n2+n4); %Bilayer to top solution
-
 
 %Fresnel Transmittance Coefficients
 t01 = 2*n0/(n0+n1);  %Substrate to base adlayer
@@ -66,30 +65,30 @@ M3_l3 = [exp(1i*phi3_l3),0;0,exp(-1*1i*phi3_l3)]; %Interstitial adlayer
 
 %Light Pathway: m01 - m1 - m12 - (j-1)[m2 - m23 - m3 - m32] - m2 - m24
 for j=0:(npoint-1)
-	M_l1 = M01*M1_l1*M12*[(M2_l1*M23*M3_l1*M32)^(j-1)]*M2_l1*M24
+	M_l1 = M01*M1_l1*M12*[(M2_l1*M23*M3_l1*M32)^(j-1)]*M2_l1*M24;
 	r_l1(j+1)=M_l1(2,1)/M_l1(1,1);
-    	t_l1(j+1)=1/M_l1(1,1);
+    t_l1(j+1)=1/M_l1(1,1);
 	
-	M_l2 = M01*M1_l2*M12*[(M2_l2*M23*M3_l2*M32)^(j-1)]*M2_l2*M24
+	M_l2 = M01*M1_l2*M12*[(M2_l2*M23*M3_l2*M32)^(j-1)]*M2_l2*M24;
 	r_l2(j+1)=M_l2(2,1)/M_l2(1,1);
-    	t_l2(j+1)=1/M_l2(1,1);
+    t_l2(j+1)=1/M_l2(1,1);
 	
-    	M_l3 = M01*M1_l3*M12*[(M2_l3*M23*M3_l3*M32)^(j-1)]*M2_l3*M24
+    M_l3 = M01*M1_l3*M12*[(M2_l3*M23*M3_l3*M32)^(j-1)]*M2_l3*M24;
 	r_l3(j+1)=M_l3(2,1)/M_l3(1,1);
-    	t_l3(j+1)=1/M_l3(1,1);
+    t_l3(j+1)=1/M_l3(1,1);
 end
 for k=1:npoint
-        R_l1(k)=norm(r_l1(k))^2;
-        T_l1(k)=norm(t_l1(k))^2*n5/n0;
+    R_l1(k)=norm(r_l1(k))^2;
+    T_l1(k)=norm(t_l1(k))^2*n4/n0;
 		
 	R_l2(k)=norm(r_l2(k))^2;
-        T_l2(k)=norm(t_l2(k))^2*n5/n0;
+    T_l2(k)=norm(t_l2(k))^2*n4/n0;
 		
 	R_l3(k)=norm(r_l3(k))^2;
-        T_l3(k)=norm(t_l3(k))^2*n5/n0;
+    T_l3(k)=norm(t_l3(k))^2*n4/n0;
 end
 
-Rbg=(((n0-n5)./(n0+n5)).^2);
+Rbg=(((n0-n4)./(n0+n4)).^2);
 CIRM_l1=R_l1./Rbg;
 CIRM_l2=R_l2./Rbg;
 CIRM_l3=R_l3./Rbg;
@@ -107,13 +106,13 @@ ylabel('IRM I/I_0')
 grid on;
 ylim([0,1.1])
 xlim([min(m)-0.5,max(m)+0.5])
-textct=sprintf('n_S_o_l=%4.3f\n n_I_A_d=%4.3f\n n_T_a_i_l%4.3f\n n_H_e_a_d=%4.3f\n n_B_A_d=%4.3f\n',n5,n4,n3,n2,n1,n0);
+textct=sprintf('n_S_o_l=%4.3f\n n_I_A_d=%4.3f\n n_L_i_p_i_d%4.3f\n n_B_A_d=%4.3f\n',n4,n3,n2,n1,n0);
 % text('Position',[20 1 0]);
 % text(min(xlim())+0.6*(max(xlim())-min(xlim())),0.8*min(ylim())+0.2*(max(ylim())-min(ylim())),textct);
 text(7,0.9,textct);
 
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3])
-opfnhead=sprintf('nSol=%4.2f,nIAd=%4.2f, nTail =%4.2f,nHead=%4.2f, nBAd = %4.2f, wavelength=%4.1fnm, HeadTailInterstitialAdlayers',n5,n4,n3,n2,n1,lambda1*1e9);
+opfnhead=sprintf('nSol=%4.2f,nIAd=%4.2f, nLipid =%4.2f, nBAd = %4.2f, wavelength=%4.1fnm, BulkInterstitialAdlayers',n4,n3,n2,n1,lambda1*1e9);
 print([opfnhead,'.png'],'-dpng');
 dlmwrite([opfnhead,'.dat'],[m,CIRM_l1],'\t');
 I_glass=4000;
@@ -122,7 +121,7 @@ Mimg(212:812,212:812)=I_glass*CIRM_l1(2);
 Mimg(412:612,412:612)=I_glass*CIRM_l1(3);
 imwrite(uint16(Mimg),[opfnhead,'.tif']);
 
-opfnhead=sprintf('nSol=%4.2f,nIAd=%4.2f, nTail =%4.2f,nHead=%4.2f, nBAd = %4.2f, wavelength=%4.1fnm, HeadTailInterstitialAdlayers',n5,n4,n3,n2,n1,lambda2*1e9);
+opfnhead=sprintf('nSol=%4.2f,nIAd=%4.2f, nLipid =%4.2f, nBAd = %4.2f, wavelength=%4.1fnm, BulkInterstitialAdlayers',n4,n3,n2,n1,lambda2*1e9);
 print([opfnhead,'.png'],'-dpng');
 dlmwrite([opfnhead,'.dat'],[m,CIRM_l2],'\t');
 I_glass=4000;
@@ -131,7 +130,7 @@ Mimg(212:812,212:812)=I_glass*CIRM_l2(2);
 Mimg(412:612,412:612)=I_glass*CIRM_l2(3);
 imwrite(uint16(Mimg),[opfnhead,'.tif']);
 
-opfnhead=sprintf('nSol=%4.2f,nIAd=%4.2f, nTail =%4.2f,nHead=%4.2f, nBAd = %4.2f, wavelength=%4.1fnm, HeadTailInterstitialAdlayers',n5,n4,n3,n2,n1,lambda3*1e9);
+opfnhead=sprintf('nSol=%4.2f,nIAd=%4.2f, nLipid =%4.2f, nBAd = %4.2f, wavelength=%4.1fnm, BulkInterstitialAdlayers',n4,n3,n2,n1,lambda3*1e9);
 print([opfnhead,'.png'],'-dpng');
 dlmwrite([opfnhead,'.dat'],[m,CIRM_l3],'\t');
 I_glass=4000;
